372cf186089bb96f725fe2eb4e3329e19b9284f5
Mohammed Noori <kmanoorib@gmail.com>
2018-08-19 02:21:31 +0000

    Added the super important all method.

--- hsw_features/lib/articles.rb
+++ hsw_features/lib/articles.rb
@@ -2,10 +2,17 @@ require 'pry'
 class Articles
   attr_accessor :title, :blurb, :url
   
+  @@all = []
+  
   def initialize (title = nil, blurb = nil, url = nil)
     @title = title
     @blurb = blurb
     @url = url
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
 end
