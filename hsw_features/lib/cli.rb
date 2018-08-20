683699e2db8eabe363717c6993b96db0c578edee
Mohammed Noori <kmanoorib@gmail.com>
2018-08-20 06:57:11 +0000

    Added methods that will allow user to select an Article to read.

--- hsw_features/lib/cli.rb
+++ hsw_features/lib/cli.rb
@@ -22,10 +22,6 @@ class CommandLineInterface
     return nil
   end
   
-  def show_proof
-    Articles.all
-  end
-  
   def run
     greetings
     scrape_for_featured_articles
@@ -35,6 +31,37 @@ class CommandLineInterface
   def scrape_selected_article(selected)
     @scraped.add_content_to_article(selected)
   end  
+##experimenting  
+  def ask_user_to_pick_article
+    "Please enter a number between 1 to 5."
+  end
+  
+  def get_user_input
+    gets.strip
+  end
+  
+  def convert_to_i(input)
+    input.to_i
+  end
+  
+  def print_selected_article(input)
+    puts scrape_selected_article(Articles.all[input-1])
+  end
+  
+  def valid_input?(input)
+    input.between?(1, 5)
+  end
+  
+  def validating_loop(input)
+    valid_input?(input) == true ? print_selected_article(input) : pick_article
+  end
+  
+  def pick_article
+    puts ask_user_to_pick_article
+    user_input = get_user_input
+    input = convert_to_i(user_input)
+    validating_loop(input)
+  end
   
 end
 
